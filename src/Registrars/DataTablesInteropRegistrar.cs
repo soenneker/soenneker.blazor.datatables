57e7e5f072d37904fdd6f78ffea5d39f5e4b3032
Jake Soenneker <jake@soenneker.com>
2024-12-20 18:53:00 -0600

    Deps

--- src/Registrars/DataTablesInteropRegistrar.cs
+++ src/Registrars/DataTablesInteropRegistrar.cs
@@ -16,9 +16,9 @@ public static class DataTablesInteropRegistrar
     /// </summary>
     public static IServiceCollection AddDataTablesInteropAsScoped(this IServiceCollection services)
     {
-        services.AddResourceLoader();
+        services.AddResourceLoaderAsScoped();
+        services.AddInteropEventListenerAsScoped();
         services.TryAddScoped<IDataTablesInterop, DataTablesInterop>();
-        services.AddInteropEventListener();
 
         return services;
     }
